12dfef004e098b24e8dc73560290edcd561f5438
Manuel Gut <gutmanuel@gmx.ch>
2024-03-26 18:01:53 +0100

    Projektdateien hinzufügen.

---
+++ P03_Nutzereingaben/P03_Nutzereingaben.cs
@@ -0,0 +1,21 @@
+﻿namespace P03_Nutzereingaben
+{
+    internal class P03_Nutzereingaben
+    {
+
+        public static void Main(string[] args)
+        {
+
+            // Abschnitt 3: Nutzereingaben & Debugging
+            Console.WriteLine("Hello from Programm 19");
+
+            Console.WriteLine("Bitte trage dein Alter ein:");
+
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Dein Alter ist also: " + age);
+        }
+
+
+    }
+}
