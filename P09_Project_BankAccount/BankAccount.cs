5712bba5f2892e59bf5fe00e95a63940db3afe62
Manuel Gut <gutmanuel@gmx.ch>
2024-03-27 17:18:37 +0100

    add P09_Project_BankAccount

---
+++ P09_Project_BankAccount/BankAccount.cs
@@ -0,0 +1,48 @@
+﻿namespace P09_Project_BankAccount
+{
+    internal class BankAccount
+    {
+
+        public string Number { get; private set; }
+
+        public string Owner { get; private set; }
+
+        public float Balance { get; private set; }
+
+        public string Pin {  get; private set; }
+
+        public BankAccount(string number, string owner, float balance, string pin)
+        {
+
+            Number = number;
+            Owner = owner;
+            Balance = balance;
+            Pin = pin;
+        }
+
+        public void MakeDeposite(float depositAmount)
+        {
+            Balance += depositAmount;
+            Console.WriteLine($"Der neue Kontostand ist CHF {Balance}.");
+        }
+
+        public void MakeWithdraw(float withdrawAmount)
+        {
+            if (Balance >= withdrawAmount)
+            {
+                Balance -= withdrawAmount;
+                Console.WriteLine($"Der neue Kontostand ist CHF {Balance}.");
+            }
+            else
+            {
+                Console.WriteLine("Unzureichender Kontostand.");
+            }
+        }
+
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Kontonummer: {Number} | Kontoinhaber: {Owner} | Kontostand: {Balance}");
+        }
+
+    }
+}
---
+++ P09_Project_BankAccount/P09_Project_BankAccount.cs
@@ -0,0 +1,64 @@
+﻿namespace P09_Project_BankAccount
+{
+    internal class P09_Project_BankAccount
+    {
+
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount("1234", "Manuel Gut", 500.0f, "1234");
+            string command = "";
+            int pinTryCounter = 1;
+            string pinInput = "";
+
+            while (true)
+            {
+                while (pinInput != bankAccount.Pin)
+                {
+                    if (pinTryCounter < 4)
+                    {
+                        Console.WriteLine($"Bitte PIN eingeben. Verbleibende Versuche {4 - pinTryCounter} :");
+                        pinInput = Console.ReadLine();
+                    }
+                    else
+                    {
+                        command = "4";
+                        Console.WriteLine("PIN zu oft falsch eingegeben. Karte wird eingezogen.");
+                        break;
+                    }
+                    pinTryCounter++;
+                }
+
+                if (command == "4")
+                {
+                    Console.WriteLine("Besten Dank und auf Wiedersehen.");
+                    break;
+
+                }
+
+                Console.WriteLine("Bitte Aktion auswählen");
+                Console.WriteLine("1. Kontostand anzeigen");
+                Console.WriteLine("2. Geld einzahlen");
+                Console.WriteLine("3. Geld abheben");
+                Console.WriteLine("4. Beenden");
+                command = Console.ReadLine();
+                if (command == "1")
+                {
+                    bankAccount.PrintBalance();
+                }
+                else if (command == "2")
+                {
+                    Console.WriteLine("Bitte den einzubezahlenden Betrag eingeben:");
+                    float amount = float.Parse(Console.ReadLine());
+                    bankAccount.MakeDeposite(amount);
+                }
+                else if (command == "3")
+                {
+                    Console.WriteLine("Bitte den Auszahlungsbetrag eingeben:");
+                    float amount = float.Parse(Console.ReadLine());
+                    bankAccount.MakeWithdraw(amount);
+
+                }
+            }
+        }
+    }
+}
