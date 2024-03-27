f8dfa3eddcba1791d6f9c3f9abbc0c86e8587b0a
Manuel Gut <gutmanuel@gmx.ch>
2024-03-27 16:09:19 +0100

    add P08_OOP_Teil1

---
+++ P08_OOP_Teil1/Car.cs
@@ -0,0 +1,40 @@
+﻿namespace P08_OOP_Teil1
+{
+    internal class Car
+    {
+        // fields with default access modifier 'private'
+        string brand;
+        string model;
+        int year;
+
+        //Properties
+        public string Brand { get; private set; }
+        public string Model { get; private set; }
+        public int Year { get; private set; }
+
+        public float ConsumptionPer100Km { get; private set; }
+
+        public Car(string brand, string model, int year, float consumptionPer100Km)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            ConsumptionPer100Km = consumptionPer100Km;
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("Car honks.");
+        }
+
+        public string GetFullInfo()
+        {
+            return $"{Brand} | {Model} | {Year}";
+        }
+
+        public float CalculateFuelConsumption(int kilometers)
+        {
+            return kilometers * ConsumptionPer100Km / 100;
+        }
+    }
+}
---
+++ P08_OOP_Teil1/P08_OOP_Teil1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08_OOP_Teil1
+{
+    internal class P08_OOP_Teil1
+    {
+
+        public static void Main(string[] args)
+        {
+            Car car1 = new Car("BMW", "d45", 2004, 6.0f);
+            car1.Honk();
+            Console.WriteLine(car1.GetFullInfo());
+            Console.WriteLine($"Das Auto verbraucht {car1.CalculateFuelConsumption(400)} Liter Benzin");
+        }
+
+    }
+}
