d314866a7975ebe4714794944894bef5e7312e50
Manuel Gut <gutmanuel@gmx.ch>
2024-04-06 12:30:41 +0200

    P14_ErweiterteSchleifenTechnik added

---
+++ P14_ErweiterteSchleifenTechnik/P14_ErweiterteSchleifenTechnik.cs
@@ -0,0 +1,52 @@
+﻿namespace P14_ErweiterteSchleifenTechnik
+{
+    internal class P14_ErweiterteSchleifenTechnik
+    {
+
+        static void Main(string[] args)
+        {
+
+            List<Product> products = new List<Product>();
+            products.Add(new Product("Bratpfannte", new List<int>() { 1, 3, 3 }));
+            products.Add(new Product("Toaster", new List<int>() { 5, 5, 4 }));
+            products.Add(new Product("Mikrowelle", new List<int>() { 3, 4, 5, 5, 5 }));
+            products.Add(new Product("Wasserkocher", new List<int>() { 2, 1 }));
+
+            // Durchlaufen aller Produkte
+            foreach (Product product in products) //<- outer loop
+            {
+                Console.WriteLine("******** Produkt: " + product.Name + " **********");
+                double ratingSum = 0;
+                int ratingAmount = product.Ratings.Count;
+
+                for (int i = 0; i < ratingAmount; i++) //<- inner loop
+                {
+                    int productRating = product.Ratings[i];
+                    Console.WriteLine($"Rating {i + 1}: {productRating}");
+                    ratingSum += productRating;
+
+                }
+                Console.WriteLine($"Durchschnittliches Rating:{Math.Round(ratingSum / ratingAmount, 2)} ");
+            }
+
+            // Demo break und continue
+            Console.WriteLine("********* Produkte filtern ***********");
+            foreach (Product product in products)
+            {
+                if (product.Name.Equals("Toaster"))
+                {
+                    Console.WriteLine("Toaster habe ich schon. Weiter zum nächsten Produkt.");
+                    continue;
+                }
+                foreach (int rating in product.Ratings)
+                {
+                    if (rating < 2)
+                    {
+                        Console.WriteLine($"Produkt {product.Name} hat mindestens eine 1-Sterne Bewertung => Abbruch");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ P14_ErweiterteSchleifenTechnik/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace P14_ErweiterteSchleifenTechnik
+{
+    internal class Product
+    {
+        public string Name { get; private set; }
+        public List<int> Ratings { get; private set; }
+
+        public Product(string name, List<int> ratings)
+        {
+            this.Name = name;
+            this.Ratings = ratings;
+        }
+
+    }
+}
